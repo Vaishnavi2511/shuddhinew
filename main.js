24954b6cb081b3c417d89d719ddc950d730f6af3
Saiyam <jindal.saiyam123@gmail.com>
2020-08-09 22:25:50 +0530

    ew

--- main.js
+++ main.js
@@ -202,7 +202,13 @@ const multerStorage = multer.diskStorage({
     })
 
 const upload = multer({
-    storage: multerStorage,
+
+    dest: function (req, file, cb) {
+        cb(null, "./public/uploads/")
+      },
+      filename: (req, file, cb) => {
+          cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+    },
     fileFilter: filter
   })
 
